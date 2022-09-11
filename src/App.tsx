6eec715d15ee3f122663d1073d8a7a8f208c4611
Mornieur <mariafernanda-coro@hotmail.com>
2022-09-11 03:56:48 -0300

    feat: adding portfolio body

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/sidebar";
+import "./styles/components/app.sass";
+
+function App() {
+  return (
+    <div id="portifolio">
+      <h1>Hello World</h1>
+      <Sidebar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
