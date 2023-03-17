6aed4ff74e6093e4166365d2296dda845b05b0e1
Mornieur <mariafernanda-coro@hotmail.com>
2023-03-17 01:18:57 -0300

    feat: create projects page

---
+++ src/ProjectsPage.tsx
@@ -0,0 +1,8 @@
+import ProjectsComponent from "./components/projects/ProjectsComponent";
+import "./styles/components/projectsPage.sass";
+
+const ProjectsPage: React.FC = () => {
+  return <ProjectsComponent />;
+};
+
+export default ProjectsPage;
