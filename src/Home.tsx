8ef91e6d09c24a01e69e1df5f3a4ab1dd19ccd63
Mornieur <mariafernanda-coro@hotmail.com>
2023-03-17 02:27:25 -0300

    feat: add telescope icon into header

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,16 @@
 import Sidebar from "./components/home/Sidebar";
 import MainContent from "./containers/MainContent";
 import "./styles/components/home.sass";
+import { GoTelescope } from "react-icons/go";
 
 const Home = () => {
   return (
     <div id="portifolio">
-      <h2>Maria Fernanda</h2>
+      <span className="homeTitle">
+        <GoTelescope className="icon" />
+        <h2 className="name">Maria Fernanda</h2>
+      </span>
+
       <Sidebar />
       <MainContent />
     </div>
