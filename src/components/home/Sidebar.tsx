b6950830ffa65fb04198b35edc641413dbedff90
Mornieur <mariafernanda-coro@hotmail.com>
2023-03-21 08:35:02 -0300

    feat: using framer motion div for photo animated

--- src/components/home/Sidebar.tsx
+++ src/components/home/Sidebar.tsx
@@ -2,11 +2,38 @@ import "../../styles/components/sidebar.sass";
 import Avatar from "../../assets/me.jpg";
 import SocialNetworks from "./SocialNetworks";
 import InformationsContainer from "./InformationsContainer";
+import { motion } from "framer-motion";
+
+const imageAnimate = {
+  offscreen: { x: -100, opacity: 0 },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: [0, 10, 0],
+    transition: { type: "spring", bounce: 0.4, duration: 1.3 },
+  },
+};
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      <img src={Avatar} alt="Maria Fernanda" />
+      <motion.div
+        className="card"
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{ once: false, amount: 0.5 }}
+        transition={{ staggerChildren: 0.7 }}
+      >
+        <motion.div
+          // whileHover={{ scale: 1.2 }}
+          // whileTap={{ scale: 0.8 }}
+          className="image-container"
+          variants={imageAnimate}
+        >
+          <img src={Avatar} alt="Maria Fernanda" />
+        </motion.div>
+      </motion.div>
+
       <p className="title">Desenvolvedora Front-end</p>
       <SocialNetworks />
       <InformationsContainer />
