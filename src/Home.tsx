f59e629791f35036ae6c41b4d4a89405fcfb6668
Mornieur <mariafernanda-coro@hotmail.com>
2023-03-22 09:56:44 -0300

    feat: add text animated

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,13 +2,17 @@ import Sidebar from "./components/home/Sidebar";
 import MainContent from "./containers/MainContent";
 import "./styles/components/home.sass";
 import { GoTelescope } from "react-icons/go";
+import { TextAnimated } from "./components/animated/TextAnimated";
 
 const Home = () => {
   return (
     <div id="portifolio">
       <span className="homeTitle">
         <GoTelescope className="icon" />
-        <h2 className="name">Maria Fernanda</h2>
+        <TextAnimated>
+          {" "}
+          <h2 className="name">Maria Fernanda</h2>
+        </TextAnimated>
       </span>
 
       <Sidebar />
